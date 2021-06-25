e2bea07c930f8e17932e665008d2f254b4f05102
s4na <appletea.umauma@gmail.com>
2021-06-26 00:04:19 +0900

    Sass導入

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script lang='ts'>
   export let name: string;
 </script>
 
@@ -11,22 +11,20 @@
       | to learn how to build Svelte apps.
 </template>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+<style lang='sass'>
+  main
+    text-align: center
+    padding: 1em
+    max-width: 240px
+    margin: 0 auto
+
+  h1
+    color: #ff3e00
+    text-transform: uppercase
+    font-size: 4em
+    font-weight: 100
+
+  @media (min-width: 640px)
+    main
+      max-width: none
 </style>
