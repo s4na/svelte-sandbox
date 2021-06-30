c3001aea5499bfa172c37a06d875c4519819cb57
s4na <appletea.umauma@gmail.com>
2021-06-30 22:46:38 +0900

    eslintの設定変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true,
+        "jest/globals": true,
     },
     "extends": [
         "airbnb-base"
@@ -12,8 +14,26 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "svelte3",
+        "jest",
     ],
+    "overrides": [
+        {
+            "files": ["*.svelte"],
+            "processor": "svelte3/svelte3",
+        },
+    ],
+    "ignorePatterns": [
+        "public/build/*",
+    ],
+    "settings": {
+        "svelte3/typescript": require("typescript"),
+        "svelte3/ignore-styles": (attributes) => attributes.lang && attributes.lang === "sass",
+    },
     "rules": {
-    }
+        "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+        "import/no-mutable-exports": "off",
+        "import/prefer-default-export": "off",
+    },
 };
