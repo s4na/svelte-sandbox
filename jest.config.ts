5209deca925e704ea49cdea4712abb983fc0bb5e
s4na <appletea.umauma@gmail.com>
2021-06-30 23:04:16 +0900

    $ yarn eslint --fix

--- jest.config.ts
+++ jest.config.ts
@@ -9,4 +9,3 @@ export default {
     '^.+\\.js$': 'babel-jest',
   },
 };
-
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 // Jest向けに設定追加
 // 参考: https://tech.actindi.net/2021/03/18/144317
 
-const sveltePreprocess = require("svelte-preprocess");
+const sveltePreprocess = require('svelte-preprocess');
 
 module.exports = {
   preprocess: sveltePreprocess({ sourceMap: true }),
